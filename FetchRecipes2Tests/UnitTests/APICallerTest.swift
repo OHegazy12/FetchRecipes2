b7e5064e06460fa16b70261f4b576b8e5e79ef91
Omar Hegazy <omarhegazy78@gmail.com>
2024-05-09 00:14:53 -0400

    Added unit tests

---
+++ FetchRecipes2Tests/UnitTests/APICallerTest.swift
@@ -0,0 +1,39 @@
+//
+//  APICallerTest.swift
+//  FetchRecipes2Tests
+//
+//  Created by Omar Hegazy on 5/9/24.
+//
+
+import XCTest
+import SwiftUI
+@testable import FetchRecipes2
+
+final class APICallerTest: XCTestCase {
+    
+    // MARK: - Test fetchDesserts() for fetching data
+    func testFetchDesserts() {
+        let expectation = self.expectation(description: "Desserts fetched successfully")
+        
+        // Call the fetchDesserts method
+        ApiCaller.fetchDesserts { mealsResponse, error in
+            XCTAssertNotNil(mealsResponse, "Meals response should not be nil")
+            XCTAssertNil(error, "Error should be nil")
+            expectation.fulfill()
+        }
+        
+        // Wait for the expectation to be fulfilled with a timeout
+        waitForExpectations(timeout: 5, handler: nil)
+    }
+    
+    // MARK: - UI test for DessertView
+    func testDessertViewLoading() {
+        let view = DessertView()
+        
+        let hostingController = UIHostingController(rootView: view)
+        _ = hostingController.view
+        
+        XCTAssertNotNil(view)
+    }
+    
+}
